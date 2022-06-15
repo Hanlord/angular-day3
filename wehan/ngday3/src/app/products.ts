478e7f94d7a8a1d8d28d657ad975713b12473da4
Hanlord <chen007@live.at>
2022-06-15 13:15:56 +0200

    structure done

---
+++ wehan/ngday3/src/app/products.ts
@@ -0,0 +1,33 @@
+export const products = [
+
+    {
+  
+      name: 'Snake',
+  
+      price: 3000,
+  
+      description: 'very long and very wet'
+  
+    },
+  
+    {
+  
+      name: 'Rabbit',
+  
+      price: 6969,
+  
+      description: 'fast and furious'
+  
+    },
+  
+    {
+  
+      name: 'Crocodile',
+  
+      price: 9000,
+  
+      description: 'aggressive + big mouth'
+  
+    }
+  
+   ];
